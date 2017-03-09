e81c76e18f07a0408ad5502cab29b77b9daff160
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2017-03-09 23:00:11 +0000

    gitignore done, config file cleaned

--- cfg/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define('DEV_ENV', true);
-
-    define('DB_NAME', 'gvrcg_db');
-    define('DB_USER', 'owen');
-    define('DB_PASS', '2R41RviX');
-    define('DB_HOST', 'localhost');
---
+++ cfg/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('DEV_ENV', true);
+
+    define('DB_NAME', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_HOST', '');
