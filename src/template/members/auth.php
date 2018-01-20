e5af4dd465dcbddfc6dcb146ccc79dc5b460a7d5
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2018-01-20 14:19:55 +0000

    Added register link to login page because Colin. Don't ask.

--- src/template/members/auth.php
+++ src/template/members/auth.php
@@ -8,6 +8,7 @@
             echo '<div class="w3-panel w3-padding w3-leftbar w3-border-red w3-pale-red">' . $error . '</div>';
         }
     ?>
+    <p>Don't have an account? <a href="<?php echo BASE_URI; ?>/members/register">Register here<a></p>
     <div class="w3-container w3-card-4">
     <form action="<?php echo BASE_URI; ?>/action/auth" method="POST">
         <div class="w3-row w3-section">
